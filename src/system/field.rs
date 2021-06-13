5eb728bf9fd6925b42655d872b9b707f288f3c36
@danielbintar <daniel.bintar@bukalapak.com>
2021-06-13 14:20:49 +0700

    yay! they are moving

--- src/system/field.rs
+++ src/system/field.rs
@@ -4,10 +4,29 @@ pub fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    mut game: ResMut<super::GameCamera>,
 ) {
     let texture_handle = asset_server.load("sprite/icon.png");
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(texture_handle.into()),
+        material: materials.add(texture_handle.clone().into()),
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            ..Default::default()
+        },
         ..Default::default()
     });
+
+    game.player.entity = Some(
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.add(texture_handle.into()),
+                transform: Transform {
+                    translation: Vec3::new(120.0, 0.0, 100.0),
+                    ..Default::default()
+                },
+                ..Default::default()
+            })
+            .insert(super::MainChar)
+            .id(),
+    );
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -46,6 +46,22 @@ pub struct Position {
     pub y: u32,
 }
 
+pub struct MainChar;
+
+#[derive(Default)]
+pub struct Player {
+    entity: Option<Entity>,
+    i: usize,
+    j: usize,
+}
+
+#[derive(Default)]
+pub struct GameCamera {
+    camera_should_focus: Vec3,
+    camera_is_focus: Vec3,
+    player: Player,
+}
+
 pub struct ButtonMaterials {
     normal: Handle<ColorMaterial>,
     hovered: Handle<ColorMaterial>,
@@ -60,5 +76,61 @@ impl FromWorld for ButtonMaterials {
             hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
             pressed: materials.add(Color::rgb(0.35, 0.75, 0.35).into()),
         }
+    }
+}
+
+const BOARD_SIZE_I: usize = 14;
+const BOARD_SIZE_J: usize = 21;
+
+const RESET_FOCUS: [f32; 3] = [
+    BOARD_SIZE_I as f32 / 2.0,
+    0.0,
+    BOARD_SIZE_J as f32 / 2.0 - 0.5,
+];
+
+pub fn setup_camera(mut commands: Commands, mut game: ResMut<GameCamera>) {
+    commands.spawn_bundle(UiCameraBundle::default());
+
+    game.camera_should_focus = Vec3::from(RESET_FOCUS);
+    game.camera_is_focus = game.camera_should_focus;
+
+    let mut camera = OrthographicCameraBundle::new_2d();
+    camera.transform.looking_at(game.camera_is_focus, Vec3::Y);
+    commands.spawn_bundle(camera);
+}
+
+pub fn move_player(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut game: ResMut<GameCamera>,
+    mut transforms: Query<&mut Transform, With<MainChar>>,
+) {
+    let mut moved = false;
+    if keyboard_input.just_pressed(KeyCode::Up) {
+        game.player.j += 30;
+        moved = true;
+    }
+    if keyboard_input.just_pressed(KeyCode::Down) {
+        if game.player.j > 30 {
+            game.player.j -= 30;
+            moved = true;
+        }
+    }
+    if keyboard_input.just_pressed(KeyCode::Right) {
+        game.player.i += 30;
+        moved = true;
+    }
+    if keyboard_input.just_pressed(KeyCode::Left) {
+        if game.player.i > 30 {
+            game.player.i -= 30;
+            moved = true;
+        }
+    }
+
+    if moved {
+        *transforms.get_mut(game.player.entity.unwrap()).unwrap() = Transform {
+            translation: Vec3::new(game.player.i as f32, game.player.j as f32, 0.0),
+            ..Default::default()
+        };
     }
 }
